65bd32be21edfc02536faf1dde7c7702104739cb
high-u <zen.crazyd@gmail.com>
2017-12-11 13:35:53 +0900

    Revert "update flowfile:/app/flows.json"
    This reverts commit aa98513c63c295593ac85b6e478061b7cd800dbb.

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ var settings = {
   },
   debugMaxLength: 1000,
   debugUseColors: true,
-  flowFile: '/app/flows.json',
-  userDir: '/app/.node-red',
-  nodesDir: '/app/.node-red/nodes',
+  flowFile: 'flows.json',
+  userDir: '/app/node-red',
+  nodesDir: '/app/node-red/nodes',
   ui: { path: "ui" },
   logging: {
     console: {
